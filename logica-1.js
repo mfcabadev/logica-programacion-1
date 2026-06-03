b9915e551ebb686b0865f565be34a23914a122f5
Manuel F. Caballero <manuelfrcaballero@gmail.com>
2026-06-03 18:16:40 -0500

    Fix console log order in logica-1.js
    Corrección de un pequeño error, la lineal 54.

--- logica-1.js
+++ logica-1.js
@@ -51,7 +51,7 @@ if (num1 === num2 && num2 === num3) {
     }
 
     console.log('=>', numero1, numero2, numero3);
-    console.log('<=', numero3, numero2, numero3);
+    console.log('<=', numero3, numero2, numero1);
 
 };
 
